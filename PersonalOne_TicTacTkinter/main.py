4b089761c64b9e6883bc8b077068858485f937c4
AnarchyEcho <anarchytrex@hotmail.com>
2024-12-27 00:16:35 +0100

    playable tictactoe

--- PersonalOne_TicTacTkinter/main.py
+++ PersonalOne_TicTacTkinter/main.py
@@ -1,12 +1,36 @@
-from tkinter import *
+import tkinter as tk
 from tkinter import ttk
 
+playerTurn = 0
+
 def main():
-  root = Tk()
+  root = tk.Tk()
+  root.geometry("250x150")
+  root.title("Tic Tac Tkinter")
   frame = ttk.Frame(root, padding=10)
   frame.grid()
-  ttk.Label(frame, text="Tic Tac Toe").grid(column=0, row=0)
-  ttk.Button(frame, command=root.destroy, text="Quit").grid(column=0, row=1)
+
+  def chkbx(btn):
+    global playerTurn
+
+    print(btn)
+    btn.config(text="O" if playerTurn == 0 else "X")
+
+    if playerTurn == 0:
+      playerTurn = 1
+    else:
+      playerTurn = 0
+
+  for i in range(3):
+    btn = ttk.Button(frame, text="")
+    btn.grid(column=0, row=i)
+    btn.configure(command=lambda id=btn: chkbx(id))
+    for y in range(2):
+      inbtn = ttk.Button(frame, text="")
+      inbtn.grid(column=y+1, row=i)
+      inbtn.configure(command=lambda inid=inbtn: chkbx(inid))
+
+  ttk.Button(frame, command=root.destroy, text="Quit").grid(column=1, row=9, pady=10)
   root.mainloop()
 
 if __name__ == '__main__':
