40376d1f54a1080a2e2e4f9a17b47e4178c28f6b
AnarchyEcho <anarchytrex@hotmail.com>
2024-12-26 21:38:44 +0100

    tkinter base implementation

--- PersonalOne_TicTacTkinter/main.py
+++ PersonalOne_TicTacTkinter/main.py
@@ -1,7 +1,13 @@
-import tkinter as tk
+from tkinter import *
+from tkinter import ttk
 
 def main():
-  print("tictactocock")
+  root = Tk()
+  frame = ttk.Frame(root, padding=10)
+  frame.grid()
+  ttk.Label(frame, text="Tic Tac Toe").grid(column=0, row=0)
+  ttk.Button(frame, command=root.destroy, text="Quit").grid(column=0, row=1)
+  root.mainloop()
 
 if __name__ == '__main__':
   main()
